31a199f052ee3d600235aec0fc9a32c3e749e605
lhysin <lhysin.main@gmail.com>
2023-05-05 15:21:40 +0900

    [2023-05-05] modified source.
    A  .github/workflows/deploy-sqs-item-consumer.yml
    M  .gitignore
    D  sqs-cws-item-consumer/index.js
    D  sqs-cws-item-consumer/package.json
    A  sqs-item-consumer/index.js
    R  sqs-cws-item-consumer/package-lock.json -> sqs-item-consumer/package-lock.json
    A  sqs-item-consumer/package.json
    A  sqs-item-consumer/serverless.yml

---
+++ sqs-item-consumer/index.js
@@ -0,0 +1,36 @@
+import { Loki } from "@loki/core";
+import { HttpTransport } from "@loki/transport-http";
+import AWS from "aws-sdk";
+import serverless from "serverless-http";
+
+const loki = new Loki({
+    transports: [
+        new HttpTransport({
+            endpoint: "http://144.24.91.88:3100/loki/api/v1/push",
+        }),
+    ],
+});
+
+const sqs = new AWS.SQS();
+const queueUrl = process.env.QUEUE_URL;
+
+const handler = async (event) => {
+    console.log(`Received message: ${JSON.stringify(event)}`);
+
+    for (const record of event.Records) {
+        try {
+            await loki.push({
+                stream: {
+                    label: "sqs",
+                },
+                values: [
+                    [Date.now(), JSON.stringify(record)],
+                ],
+            });
+        } catch (error) {
+            console.error(`Error sending log to Loki: ${error}`);
+        }
+    }
+};
+
+export const logSqsMessages = serverless(handler);
